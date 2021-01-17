73e03cbe2494b9b1590d2835a30f82d65c06cb97
Maksym Rybak <rybak.maksym@gmail.com>
2021-01-17 22:20:39 +0100

    example of get it all, getting all related data at startup

--- src/app/products/product-list-alt/product-detail.component.ts
+++ src/app/products/product-list-alt/product-detail.component.ts
@@ -21,5 +21,12 @@ export class ProductDetailComponent {
     })
   );
 
+  productSuppliers$ = this.productService.selectedProductSuppliers$.pipe(
+    catchError((err) => {
+      this.errorMessageSubject.next(err);
+      return EMPTY;
+    })
+  );
+
   constructor(private productService: ProductService) {}
 }
